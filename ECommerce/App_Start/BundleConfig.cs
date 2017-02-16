6804d6cecc735fefa6335596bc30f011ef42bebf
Pablo Gomez <soidneoqgmail.com>
2017-02-15 20:07:39 -0500

    datepicker

--- ECommerce/App_Start/BundleConfig.cs
+++ ECommerce/App_Start/BundleConfig.cs
@@ -20,12 +20,15 @@ namespace ECommerce
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/moment.js",
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap-datetimepicker.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-datepicker.css")); 
         }
     }
 }
